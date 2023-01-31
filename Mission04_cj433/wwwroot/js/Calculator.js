2d01ef6ddfe43c6c2b49dda37f319c25dab2d782
Colby Jensen <cj433@byu.edu>
2023-01-30 22:12:24 -0700

    Mission 4 Submission.

---
+++ Mission04_cj433/wwwroot/js/Calculator.js
@@ -0,0 +1,41 @@
+﻿/*Grade Calculator Function*/
+
+$("#btnCalc").click(function calc() {
+    var assignment = $("#assignment").val();
+    var group = $("#group").val();
+    var quiz = $("#quiz").val();
+    var mid = $("#mid").val();
+    var final = $("#final").val();
+    var intex = $("#intex").val();
+
+    var totalGrade = ((assignment * .50) + (group * .10) + (quiz * .10) + (mid * .10) + (final * .10) + (intex * .10));
+
+    if (totalGrade >= 94) {
+        letterGrade = "A";
+    } else if (totalGrade >= 90) {
+        letterGrade = "A-";
+    } else if (totalGrade >= 87) {
+        letterGrade = "B+";
+    } else if (totalGrade >= 84) {
+        letterGrade = "B";
+    } else if (totalGrade >= 80) {
+        letterGrade = "B-";
+    } else if (totalGrade >= 77) {
+        letterGrade = "C+";
+    } else if (totalGrade >= 74) {
+        letterGrade = "C";
+    } else if (totalGrade >= 70) {
+        letterGrade = "C-";
+    } else if (totalGrade >= 67) {
+        letterGrade = "D+";
+    } else if (totalGrade >= 64) {
+        letterGrade = "D";
+    } else if (totalGrade >= 60) {
+        letterGrade = "D-";
+    } else {
+        letterGrade = "E";
+    }
+
+    $("#letter").html("Final Grade: " + letterGrade);
+    $("#percent").html("Percent: " + totalGrade.toFixed(2) + "%");
+})
